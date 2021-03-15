456e353f78882856031e88a6c4b904305a04f7b2
Kona-Yuki-SAN <6841207+p-snow@users.noreply.github.com>
2021-03-15 09:22:13 +0900

    make framework for managing confidentials

--- tangle.el
+++ tangle.el
@@ -1,7 +1,14 @@
 (require 'org)
 
 (let* ((dotfiles-path (expand-file-name "./"))
-       (org-files (directory-files dotfiles-path nil "\\.org$")))
+       (org-files (directory-files dotfiles-path nil "\\.org\\(\\.gpg\\)?$")))
   (dolist (org-file org-files)
     (unless (member org-file '("README.org"))
-      (org-babel-tangle-file (expand-file-name org-file dotfiles-path)))))
+      (mapc (lambda (tangled-file)
+              (when (string-match-p "\\.password-store" tangled-file)
+                (call-process-shell-command
+                 (format "gpg -o %1$s -r %2$s -e %3$s; rm -f %3$s"
+                         (concat tangled-file ".gpg")
+                         user-full-name
+                         tangled-file))))
+            (org-babel-tangle-file (expand-file-name org-file dotfiles-path))))))
