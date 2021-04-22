3c14e83aa5a79863067026a7b09c9b0b728d0f62
Kona-Yuki-SAN <6841207+p-snow@users.noreply.github.com>
2021-04-22 15:43:55 +0900

    add support encrypted entry in tangle files

--- tangle.el
+++ tangle.el
@@ -1,14 +1,19 @@
 (require 'org)
 
+(remove-hook 'org-babel-pre-tangle-hook
+             'save-buffer)
+
 (let* ((dotfiles-path (expand-file-name "./"))
        (org-files (directory-files dotfiles-path nil "\\.org\\(\\.gpg\\)?$")))
   (dolist (org-file org-files)
     (unless (member org-file '("README.org"))
-      (mapc (lambda (tangled-file)
-              (when (string-match-p "\\.password-store" tangled-file)
-                (call-process-shell-command
-                 (format "gpg -o %1$s -r %2$s -e %3$s; rm -f %3$s"
-                         (concat tangled-file ".gpg")
-                         user-full-name
-                         tangled-file))))
-            (org-babel-tangle-file (expand-file-name org-file dotfiles-path))))))
+      (with-current-buffer (find-file-noselect org-file)
+        (org-decrypt-entries)
+        (mapc (lambda (target-file)
+                (when (string-match-p "\\.password-store" target-file)
+                  (call-process-shell-command
+                   (format "gpg -o %1$s -r %2$s -e %3$s; rm -f %3$s"
+                           (concat target-file ".gpg")
+                           user-full-name
+                           target-file))))
+              (mapcar #'expand-file-name (org-babel-tangle)))))))
