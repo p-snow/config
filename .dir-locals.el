36c9038db8aa3800cadb2297f16980fc088b0d78
p-snow <6841207+p-snow@users.noreply.github.com>
2021-12-08 12:10:42 +0900

    add gimmick to start tangling automatically when org file is saved

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,7 @@
-((nil . ((compile-command . "make tangle"))))
+((nil . ((compile-command . "make tangle")))
+ (org-mode . ((eval . (defun dotfiles/compile ()
+                        "Compile this repository and conceal compilation buffer."
+                        (funcall #'compile "make tangle")
+                        (quit-window)))
+              (eval . (add-hook 'after-save-hook
+                                #'dotfiles/compile nil t)))))
