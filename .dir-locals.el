9c65d7fbe8c0302159b24450c741159abcb3babf
p-snow <6841207+p-snow@users.noreply.github.com>
2022-03-24 16:05:05 +0900

    fix a tangling issue

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,7 @@
-((nil . ((compilation-directory "./")
-         (compile-command . "make tangle")))
+((nil . ((compile-command . "make tangle")))
  (org-mode . ((eval . (defun dotfiles/compile ()
                         "Compile this repository and conceal compilation buffer."
-                        (let ((compilation-directory "./"))
-                          (funcall #'compile "make tangle")
-                          (quit-window))))
+                        (funcall #'compile "make tangle")
+                        (quit-window)))
               (eval . (add-hook 'after-save-hook
                                 #'dotfiles/compile nil t)))))
