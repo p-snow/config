8bbe1d83c3d82b7ce44fc98f82161405a69e8ee4
p-snow <6841207+p-snow@users.noreply.github.com>
2022-07-02 22:48:57 +0900

    suppress confirmation when invoking unsafe functions

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,3 @@
 ((nil . ((compile-command . "make tangle")))
- (org-mode . ((eval . (defun dotfiles/compile ()
-                        "Compile this repository and conceal compilation buffer."
-                        (funcall #'compile "make tangle")
-                        (quit-window)))
-              (eval . (add-hook 'after-save-hook
-                                #'dotfiles/compile nil t)))))
+ (org-mode . ((eval . (add-hook 'after-save-hook
+                                #'my/compile-default-command nil t)))))
