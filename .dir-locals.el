2bc7490d911713263d437a7162450dd212bffec0
p-snow <6841207+p-snow@users.noreply.github.com>
2023-07-13 17:23:41 +0900

    add a setting to be distinct project

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ((nil . ((compile-command . "make tangle")
-         (magit-wip-mode . t)))
+         (magit-wip-mode . t)
+         (project-vc-merge-submodules . nil)))
  (org-mode . ((auto-save-visited-mode . nil)
               (eval . (add-hook 'after-save-hook
                                 #'my/compile-default-command nil t)))))
