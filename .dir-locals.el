60186ff44494a8f25810ade7ce90b2ecfb5b553c
p-snow <6841207+p-snow@users.noreply.github.com>
2023-12-15 15:57:35 +0900

    move variable set statements from .dir-locals.el to dotfiles.org as local variables

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,1 @@
-((nil . ((compile-command . "make tangle")
-         (project-vc-merge-submodules . nil)))
- (org-mode . ((auto-save-visited-mode . nil)
-              (eval . (add-hook 'after-save-hook
-                                #'my/compile-default-command nil t))
-              (eval . (remove-hook 'find-file-hook
-                                   (lambda () (setq-local buffer-save-without-query t)))))))
+((nil . ((project-vc-merge-submodules . nil))))
