b33020a233c671e1a524b7c22c10aaa31d52be16
p-snow <6841207+p-snow@users.noreply.github.com>
2024-05-26 21:05:03 +0900

    Change to rely on Guix Home Configuration for Bash settings.

--- tangle.el
+++ tangle.el
@@ -6,7 +6,8 @@
 ;; top directory for all tangled files
 (setq tangle/home "~/data/ghq/github.com/p-snow/dotfiles/")
 
-(setq tangle/user-emacs-directory (expand-file-name ".config/emacs" tangle/home)
+(setq tangle/config-dir (expand-file-name ".config" tangle/home)
+      tangle/user-emacs-directory (expand-file-name ".config/emacs" tangle/home)
       tangle/emacs-sitelisp-dir (expand-file-name "site-lisp" tangle/user-emacs-directory)
       tangle/emacs-config-dir (expand-file-name "lisp" tangle/user-emacs-directory)
       tangle/emacs-etc-dir (expand-file-name "etc" tangle/user-emacs-directory)
