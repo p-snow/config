f818553599a6eaa7ec347f897050743e6a7d6bdb
p-snow <6841207+p-snow@users.noreply.github.com>
2024-05-31 13:47:42 +0900

    Sort out "tangle/*" variables for tangling destinations.

--- tangle.el
+++ tangle.el
@@ -1,19 +1,8 @@
 (require 'org)
 (require 'ob-core)
 (require 'ob-shell)
+(require 'ob-python)
 (require 'ob-ruby)
-
-;; top directory for all tangled files
-(setq tangle/home "~/data/ghq/github.com/p-snow/dotfiles/")
-
-(setq tangle/config-dir (expand-file-name ".config" tangle/home)
-      tangle/user-emacs-directory (expand-file-name ".config/emacs" tangle/home)
-      tangle/emacs-sitelisp-dir (expand-file-name "site-lisp" tangle/user-emacs-directory)
-      tangle/emacs-config-dir (expand-file-name "lisp" tangle/user-emacs-directory)
-      tangle/emacs-etc-dir (expand-file-name "etc" tangle/user-emacs-directory)
-      tangle/emacs-var-dir (expand-file-name "var" tangle/user-emacs-directory)
-      tangle/local-bin-dir (expand-file-name ".local/bin/" tangle/home)
-      tangle/proj-dir (expand-file-name "./"))
 
 ;; (load-file (expand-file-name "org-babel-config.el" tangle/emacs-config-dir))
 (load-file "./org-babel-config.el")
