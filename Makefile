11662b164a2aec8bc89202f56e4d8040ecce6553
p-snow <6841207+p-snow@users.noreply.github.com>
2024-09-22 22:49:21 +0900

    Add blog resources

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ EMACS	  = emacs
 TANGLE_EL	= ./tangle.el
 DETANGLE_EL	= ./detangle.el
 PUBLISH_EL	= ./publish.el
+BLOG_EL	= ./blog.el
 EARLYINIT_EL	= ~/.config/emacs/early-init.el
 BABEL_CONF_EL	= ~/.config/emacs/lisp/org-babel-config.el
 
@@ -13,6 +14,9 @@ tangle: $(TANGLE_EL) $(EARLYINIT_EL) $(BABEL_CONF_EL)
 	$(EMACS) -Q --batch -l $(word 2,$^) -l $(word 3,$^) -l $<
 
 publish: $(PUBLISH_EL)
+	$(EMACS) -Q --batch -l $<
+
+blog: $(BLOG_EL)
 	$(EMACS) -Q --batch -l $<
 
 .PHONY: tangle
---
+++ blog.el
@@ -0,0 +1,61 @@
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; (package-install 'htmlize)
+(package-install 'org-static-blog)
+
+(require 'ox-publish)
+
+(setq make-backup-files nil)
+
+
+(require 'org-static-blog)
+(setq org-static-blog-publish-title ".dot life")
+(setq org-static-blog-publish-url "https://www.p-snow.org")
+(setq org-static-blog-publish-directory "./public/")
+(setq org-static-blog-posts-directory "./content/posts/")
+(setq org-static-blog-drafts-directory "./content/drafts/")
+(setq org-static-blog-enable-tags t)
+(setq org-static-blog-use-preview t)
+(setq org-static-blog-preview-start "<preview>")
+(setq org-static-blog-preview-end "</preview>")
+(setq org-static-blog-preview-ellipsis "(read more...)")
+(setq org-static-blog-preview-link-p t)
+(setq org-static-blog-page-header
+      (concat "<meta charset=\"utf-8\">"
+              "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">"
+              "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+              "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.css\">"
+              "<link rel=\"stylesheet\" href=\"./assets/css/style.css\">"))
+(setq org-static-blog-page-preamble
+      (concat "<header><nav>"
+              "<a href=\"https://www.p-snow.org\">Home</a>"
+              "<a href=\"https://www.p-snow.org/config/dotfiles.html#gnu-emacs\">Emacs Config</a>"
+              "<a href=\"https://www.p-snow.org/tag-english.html\">Posts</a>"
+              "<a href=\"https://www.p-snow.org/tag-japanese.html\">日本語記事</a>"
+              "<a href=\"https://www.p-snow.org/about.html\">About</a>"
+              "<a href=\"https://www.p-snow.org/rss.xml\">RSS</a>"
+              "</nav><h1>.dot life</h1><p>by p-snow</p></header>"))
+(setq org-static-blog-no-post-tag "nonpost")
+(setq org-static-blog-rss-excluded-tag "nonrss")
+
+(setq org-export-with-toc nil)
+(setq org-export-with-section-numbers nil)
+(setq org-export-with-broken-links 'mark
+      org-export-use-babel nil)
+
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+(org-static-blog-publish-file "./content/drafts/about.org")
+(org-static-blog-publish)
+
+(message "Build complete!")
