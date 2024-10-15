8b873457186a62d1ead4d5f5d453c1102a501b96
p-snow <6841207+p-snow@users.noreply.github.com>
2024-10-16 00:43:21 +0900

    Added a new diary post

--- diary.el
+++ diary.el
@@ -36,9 +36,9 @@
 (setq org-static-blog-page-preamble
       (concat "<header><nav>"
               "<a href=\"https://www.p-snow.org/config/diary\">ホーム</a>"
-              "<a href=\"https://www.p-snow.org/config/diary/about.html\">こなゆき</a>"
+              "<a href=\"https://www.p-snow.org/config/diary/about.html\">あばうと</a>"
               "<a href=\"https://www.p-snow.org/config/diary/rss.xml\">RSS</a>"
-              "</nav><h1>ティンカラー日記</h1><p>こなゆき</p></header>"))
+              "</nav><h1>ティンカラー日記</h1><p>こなゆき(a.k.a. p-snow)</p></header>"))
 (setq org-static-blog-no-post-tag "nonpost")
 (setq org-static-blog-rss-excluded-tag "nonrss")
 
